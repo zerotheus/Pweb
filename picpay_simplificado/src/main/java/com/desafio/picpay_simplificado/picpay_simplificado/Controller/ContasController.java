6984b97ca3a23e20590983be765a431ba71e389f
Ntheus <104659816+zerotheus@users.noreply.github.com>
2023-08-22 16:29:09 -0300

    user controler -> ContasController

---
+++ picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Controller/ContasController.java
@@ -0,0 +1,34 @@
+package com.desafio.picpay_simplificado.picpay_simplificado.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaPF;
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaPJ;
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.Users;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/User")
+@AllArgsConstructor
+public class ContasController {
+
+    @PostMapping("/Register") // usado para debug
+    public void createUser(@RequestBody Users user) {
+        System.out.println(user);
+    }
+
+    @PostMapping("/CriaPF")
+    public void createContaPF(@RequestBody ContaPF conta) {
+        System.out.println(conta);
+    }
+
+    @PostMapping("/CriaPJ")
+    public void createContaPJ(@RequestBody ContaPJ contaPj) {
+        System.out.println(contaPj);
+    }
+
+}
