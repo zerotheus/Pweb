43965e13eda0ed02803613ac8e730a0f4520c060
Ntheus <104659816+zerotheus@users.noreply.github.com>
2023-08-30 17:03:33 -0300

    esboco de valida transacao

--- picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Services/TransacaoServices.java
+++ picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Services/TransacaoServices.java
@@ -1,6 +1,21 @@
 package com.desafio.picpay_simplificado.picpay_simplificado.Services;
 
-//@Service
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.Transacao;
+import com.desafio.picpay_simplificado.picpay_simplificado.Repository.ContaRepository;
+import com.desafio.picpay_simplificado.picpay_simplificado.Repository.TransacaoRepository;
+
+@Service
 public class TransacaoServices {
+
+    @Autowired
+    private ContaRepository contaRepository;
+    private TransacaoRepository transacaoRepository;
+
+    public void validaTransacao(Transacao transacao) {
+
+    }
 
 }
