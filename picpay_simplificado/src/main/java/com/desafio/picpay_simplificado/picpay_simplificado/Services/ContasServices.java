d65107b1b8af5bf331822cabb4f950fe0dbc7596
Ntheus <matheusoliveira.7313@gmail.com>
2023-08-31 02:01:31 -0300

    busca de contas

--- picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Services/ContasServices.java
+++ picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Services/ContasServices.java
@@ -1,9 +1,11 @@
 package com.desafio.picpay_simplificado.picpay_simplificado.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaModel;
 import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaPF;
 import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaPJ;
 import com.desafio.picpay_simplificado.picpay_simplificado.Repository.ContaRepository;
@@ -25,6 +27,21 @@ public class ContasServices {
     public ContaPJ criaContaPJ(ContaPJ contaPJ) {
         usersRepository.save(contaPJ.getUser());
         return contaRepository.save(contaPJ);
+    }
+
+    public ResponseEntity<ContaModel> encontraContaPeloId(Long id) {
+        return contaRepository.findById(id).map(conta -> ResponseEntity.ok().body(conta))
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    public ResponseEntity<ContaPF> encontraContaPFPeloId(Long id) {
+        return contaRepository.findContaPFbyId(id).map(contaPF -> ResponseEntity.ok().body(contaPF))
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    public ResponseEntity<ContaPJ> encontraContaPJPeloId(Long id) {
+        return contaRepository.findContaPJbyId(id).map(contaPJ -> ResponseEntity.ok().body(contaPJ))
+                .orElse(ResponseEntity.badRequest().build());
     }
 
 }
