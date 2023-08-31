76413fcae7702b51cf65957d60d3f21df8d136a5
Ntheus <matheusoliveira.7313@gmail.com>
2023-08-31 02:01:17 -0300

    Metodos de validacao de contas da transacao

--- picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Services/TransacaoServices.java
+++ picpay_simplificado/src/main/java/com/desafio/picpay_simplificado/picpay_simplificado/Services/TransacaoServices.java
@@ -1,21 +1,43 @@
 package com.desafio.picpay_simplificado.picpay_simplificado.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.desafio.picpay_simplificado.picpay_simplificado.Model.Transacao;
-import com.desafio.picpay_simplificado.picpay_simplificado.Repository.ContaRepository;
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaModel;
+import com.desafio.picpay_simplificado.picpay_simplificado.Model.ContaPF;
 import com.desafio.picpay_simplificado.picpay_simplificado.Repository.TransacaoRepository;
 
 @Service
 public class TransacaoServices {
 
     @Autowired
-    private ContaRepository contaRepository;
+    private ContasServices contasServices;
     private TransacaoRepository transacaoRepository;
 
-    public void validaTransacao(Transacao transacao) {
+    public void validaTransacao(Long remetenteId, Long destinatarioId, double valor) throws Exception {
+        adicionaRemetente(remetenteId);
+        adicionaDestinatario(destinatarioId);
+    }
 
+    private ContaPF adicionaRemetente(Long remetenteId) throws Exception {
+        ResponseEntity<ContaPF> resultadoDaBusca = contasServices.encontraContaPFPeloId(remetenteId);
+        if (!resultadoDaBusca.getStatusCode().is2xxSuccessful()) {
+            throw new Exception("Conta nao encontrada ou nao pode realizar este tipo de operacao");
+        }
+        return resultadoDaBusca.getBody();
+    }
+
+    private ContaModel adicionaDestinatario(Long destinatarioId) throws Exception {
+        ResponseEntity<ContaModel> resultadoDaBusca = contasServices.encontraContaPeloId(destinatarioId);
+        if (!resultadoDaBusca.getStatusCode().is2xxSuccessful()) {
+            throw new Exception("Conta nao encontrada");
+        }
+        return resultadoDaBusca.getBody();
+    }
+
+    private boolean foiAutorizada() {
+        return false;
     }
 
 }
